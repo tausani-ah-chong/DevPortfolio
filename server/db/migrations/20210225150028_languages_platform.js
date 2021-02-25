0e597f2499edc221e4264f2032df0b44c1f874c9
steve-kim-96 <soungku@hotmail.com>
2021-02-25 15:52:48 +1300

    Edit developer migration

--- server/db/migrations/20210225150028_languages_platform.js
+++ server/db/migrations/20210225150028_languages_platform.js
@@ -4,15 +4,12 @@ exports.up = function (knex) {
     table.dropColumn('platforms')
     table.dropColumn('locations')
     table.dropColumn('projects_id')
-    table.integer('project_id').references('projects.id')
-    table.integer('language_id').references('languages.id')
-    table.integer('platform_id').references('platforms.id')
-    table.string('location')
+    table.dropColumn('uuid')
+    table.string('email')
+    table.unique('email')
   })
 }
 
 exports.down = function (knex) {
-  // return knex.schema.table('developers', table => {
-  //   table.dropColumns('project_id', 'language_id', 'platform_id', 'location')
-  // })
+
 }
