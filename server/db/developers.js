25327010f2436c3e5370a04b44b1bdca7e4c1ac3
steve-kim-96 <soungku@hotmail.com>
2021-02-25 16:37:10 +1300

    saving current progress

--- server/db/developers.js
+++ server/db/developers.js
@@ -1,9 +1,27 @@
 const connection = require('./connection')
 
 module.exports = {
-  getDevelopers
+  getDevelopers,
+  devProjects
 }
 
-function getDevelopers (db = connection) {
+function getDevelopers(db = connection) {
   return db('developers').select()
+}
+
+function devProjects (id, db = connection) {
+  return db('developers')
+    .leftJoin('developersProjects', 'developersProjects.developer_id', 'developers.id')
+    .leftJoin('projects', 'developersProjects.project_id', 'projects.id')
+    .select('profile_picture as profilePic', 'first_name as firstName', 'last_name as lastName', 'pronoun', 'bio', 'projects.image as projectImage', 'projects.name as projectName', 'link', 'email')
+    .where('developers.id', id)
+    .then(result => {
+      const dev = result[0]
+      return {
+        profilePic: dev.profilePic,
+        firstName: dev.firstName,
+        lastName: dev.lastName,
+        pronoun: dev.pronoun
+      }
+    })
 }
