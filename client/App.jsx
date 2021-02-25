29a21c9139df84b407bcc7674c13171a0c75d31e
tausani-ah-chong <tausani93@gmail.com>
2021-02-25 17:21:30 +1300

    rearrange app components load order

--- client/App.jsx
+++ client/App.jsx
@@ -3,11 +3,14 @@ import { Route } from 'react-router-dom'
 
 import Explore from './components/Explore'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
+
 const App = () => {
   return (
     <>
-      <Route to='/' component={Explore} />
       <Route to='/' component={Nav} />
+      <Route to='/' component={Explore} />
+      <Route to='/' component={Footer} />
     </>
   )
 }
