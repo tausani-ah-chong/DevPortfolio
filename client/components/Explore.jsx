ea93b7041c54865ea0462ffa76ad13e216e13ca0
Multi Ah Foon <35914987+multiahfoon@users.noreply.github.com>
2021-02-26 16:52:38 +1300

    add profile images

--- client/components/Explore.jsx
+++ client/components/Explore.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Explore () {
   return (
     <h1>Explore Page!</h1>
+
   )
 }
 
--- server/db/seeds/2-projects.js
+++ server/db/seeds/2-projects.js
@@ -1,8 +1,24 @@
 exports.seed = function (knex) {
   return knex('projects').insert([
-    { id: 1, name: 'rowValue1', image: '', link: '' },
-    { id: 2, name: 'rowValue1', image: '', link: '' },
-    { id: 3, name: 'rowValue2', image: '', link: '' },
-    { id: 4, name: 'rowValue3', image: '', link: '' }
+    { id: 1, name: 'rowValue1', image: '/images/project-img/alexey-turenkov-LZp01Y9g-zo-unsplash-min.jpg', link: '' },
+    { id: 2, name: 'rowValue1', image: '/images/project-img/altumcode-oZ61KFUQsus-unsplash-min.jpg', link: '' },
+    { id: 3, name: 'rowValue2', image: '/images/project-img/anamnesis33-PtsTG1BBOwU-unsplash-min.jpg', link: '' },
+    { id: 4, name: 'rowValue3', image: '/images/project-img/andrew-ruiz-Cehlo_2ZbTc-unsplash-min.jpg', link: '' }
+    { id: 5, name: 'rowValue3', image: '/images/project-img/annika-gordon-xqSDI58vkUY-unsplash-min.jpg', link: '' }
+    { id: 6, name: 'rowValue3', image: '/images/project-img/arif-riyanto-G1N9kDHqBrQ-unsplash-min.jpg', link: '' }
+    { id: 7, name: 'rowValue3', image: '/images/project-img/aris-rovas-A63PhHEJdoo-unsplash-min.jpg', link: '' }
+    { id: 8, name: 'rowValue3', image: '/images/project-img/arno-senoner-2P20Xhpw1KE-unsplash-min.jpg', link: '' }
+    { id: 9, name: 'rowValue3', image: '/images/project-img/bianca-ackermann-8gHiadIS0Ks-unsplash-min.jpg', link: '' }
+    { id: 10, name: 'rowValue3', image: '/images/project-img/calvin-3gYPjALn7pA-unsplash-min.jpg', link: '' }
+    { id: 11, name: 'rowValue3', image: '/images/project-img/campaign-creators-e6n7uoEnYbA-unsplash-min.jpg', link: '' }
+    { id: 12, name: 'rowValue3', image: '/images/project-img/danny-protas-iOfci0RaLdg-unsplash-min.jpg', link: '' }
+    { id: 13, name: 'rowValue3', image: '/images/project-img/dimitry-zub-F4WWTBUMZ9I-unsplash-min.jpg', link: '' }
+    { id: 14, name: 'rowValue3', image: '/images/project-img/dmytro-pidhrushnyi-MNDUiJh_Go8-unsplash-min.jpg', link: '' }
+    { id: 15, name: 'rowValue3', image: '/images/project-img/emilie-farris-8lTiMWjiszs-unsplash-min.jpg', link: '' }
+    { id: 16, name: 'rowValue3', image: '/images/project-img/frankie-lopez-Dh7X6RjjNCs-unsplash-min.jpg', link: '' }
+    { id: 17, name: 'rowValue3', image: '/images/project-img/frankie-lopez-mRwOOCTL0HQ-unsplash-min.jpg', link: '' }
+    { id: 18, name: 'rowValue3', image: '/images/project-img/gabriel-gurrola-u6BPMXgURuI-unsplash-min.jpg', link: '' }
+    { id: 19, name: 'rowValue3', image: '/images/project-img/jan-mayer-sAQ-vjVey20-unsplash-min.jpg', link: '' }
+    { id: 20, name: 'rowValue3', image: '/images/project-img/joey-banks-YApiWyp0lqo-unsplash-min.jpg', link: '' }
   ])
 }
