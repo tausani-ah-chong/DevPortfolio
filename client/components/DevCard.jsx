2989f71906ad6afc74bb45b84f264a28e7347e98
tausani-ah-chong <tausani93@gmail.com>
2021-02-28 12:39:13 +1300

    add stock image for devcard if no projects

--- client/components/DevCard.jsx
+++ client/components/DevCard.jsx
@@ -4,9 +4,13 @@ function DevCard ({ dev }) {
   return (
     <>
       <div className=" relative w-full h-64 bg-green-300 rounded-md">
-        <img src={dev.projects[0].projectImage} />
+        {
+          dev.projects.length
+            ? <img src={dev.projects[0].projectImage} />
+            : <img src='/images/project-img/gabriel-gurrola-u6BPMXgURuI-unsplash-min.jpg' />
+        }
         <div className=" absolute bottom-0 h-20 w-full bg-transparent flex flex-row">
-          <img src={dev.profilePic} className=" rounded-full bg-red-400 w-14 h-14 m-4"></img>
+          <img src={dev.profilePic} className=" rounded-full bg-red-400 w-14 h-14 m-4" />
           <h4 className="self-center ml-6">{dev.firstName}</h4>
         </div>
       </div>
