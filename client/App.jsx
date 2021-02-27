718bae4718663c567c9483dc734bfa9ce2062fa2
tausani-ah-chong <tausani93@gmail.com>
2021-02-27 15:31:28 +1300

    add template to css route

--- client/App.jsx
+++ client/App.jsx
@@ -5,14 +5,16 @@ import Explore from './components/Explore'
 import Nav from './components/Nav'
 import SearchBar from './components/SearchBar'
 import Banner from './components/Banner'
+import Template from './components/Template'
 
 const App = () => {
   return (
     <>
-      <Route to='/' component={Nav} />
-      <Route to='/' component={Banner} />
-      <Route to='/' component={SearchBar} />
-      <Route to='/' component={Explore} />
+      <Route exact path='/' component={Nav} />
+      <Route exact path='/' component={Banner} />
+      <Route exact path='/' component={SearchBar} />
+      <Route exact path='/' component={Explore} />
+      <Route exact path='/css' component={Template} />
     </>
   )
 }
