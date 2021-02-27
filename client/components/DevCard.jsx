5e2e488dc8058675c7a2572848d4ba3201da5a92
tausani-ah-chong <tausani93@gmail.com>
2021-02-27 13:17:18 +1300

    update devcard profilepic to camel case

--- client/components/DevCard.jsx
+++ client/components/DevCard.jsx
@@ -5,7 +5,7 @@ function DevCard ({ dev }) {
     <>
       <div className=" relative w-full h-64 bg-green-300 rounded-md">
         <div className=" absolute bottom-0 h-20 w-full bg-transparent flex flex-row">
-          <img src={dev.profile_picture} className=" rounded-full bg-red-400 w-14 h-14 m-4"></img>
+          <img src={dev.profilePicture} className=" rounded-full bg-red-400 w-14 h-14 m-4"></img>
           {/* <h4 className="self-center ml-6">{dev.first_name}</h4> */}
         </div>
       </div>
