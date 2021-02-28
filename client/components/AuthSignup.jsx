dd7381924faa13060c3c4b94a676b5370ed1bf81
steve-kim-96 <soungku@hotmail.com>
2021-02-28 20:08:17 +1300

    update changes

--- client/components/AuthSignup.jsx
+++ client/components/AuthSignup.jsx
@@ -1,48 +1,50 @@
 import React, { useRef, useState } from 'react'
-// import { useAuth } from '../contexts/AuthContext'
+
+import { useAuth } from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
 function AuthSignup () {
-  // const { signup } = useAuth()
-  // const emailRef = useRef()
-  // const passwordRef = useRef()
-  // const passwordConfirmRef = useRef()
-  // const [error, setError] = useState('')
-  // const [loading, setLoading] = useState(false)
-  // const history = useHistory()
+  const { signup } = useAuth()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
 
-  // async function signupHandler (e) {
-  //   e.preventDefault()
+  async function signupHandler (e) {
+    e.preventDefault()
 
-  //   if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-  //     setError('Passwords do not match')
-  //   }
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      setError('Passwords do not match')
+    }
 
-  //   try {
-  //     setError('')
-  //     setLoading(true)
-  //     await signup(emailRef.current.value, passwordRef.current.value)
-  //     history.push('/')
-  //   } catch {
-  //     setError('Failed to create an account')
-  //   }
-  //   setLoading(false)
-  // }
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+      history.push('/')
+    } catch {
+      setError('Failed to create an account')
+    }
+    setLoading(false)
+  }
 
   return (
     <>
       <h1>Sign up!</h1>
-      <form >
+      <form onSubmit={signupHandler}>
+        {error && alert(error)}
         <label htmlFor='email'>Email</label>
-        <input id='email' type='email' name='email' required/>
+        <input id='email' type='email' ref={emailRef} name='email' required></input>
 
         <label htmlFor='password'>Password</label>
-        <input id='password' type='password' name='password' required/>
+        <input id='password' type='password' ref={passwordRef} name='password' required></input>
 
         <label htmlFor='confirmPassword'>Confirm Password</label>
-        <input id='confirmPassword' type='password' name='confirmPassword' required/>
+        <input id='confirmPassword' type='password' ref={passwordConfirmRef} name='confirmPassword' required></input>
 
-        <button>Sign Up</button>
+        <button disable={loading}>Sign Up</button>
 
       </form>
 
