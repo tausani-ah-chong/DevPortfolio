6fc8258bb685e47a2dc37c1db9b456af3cd2e063
Christo Karadjov <clkaradjov@gmail.com>
2021-02-28 23:50:39 +1300

    items centered within grid

--- client/components/MoreInfoSignup.jsx
+++ client/components/MoreInfoSignup.jsx
@@ -8,29 +8,35 @@ function MoreInfoSignup () {
 
   return (
     <>
-      <div className="w-full h-auto bg-yellow-100 flex items-center">
-        <div className=" border-black border-2 w-auto mx-auto">
+      <div className="w-full h-auto flex items-center">
+        <div className=" w-auto mx-auto my-20">
           <form onSubmit={handleSubmit}>
 
             <p className="text-center text-3xl font-semibold ">What programming Languages do you know?</p>
             <span className="flex flex-row">
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-javascript-plain text-9xl text-white"></i>
-                <label htmlFor='JS'>JS</label>
-                <input id='JS' type="checkbox" value='JS' />
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6">
+                <ul className="text-center">
+                  <li><i className="devicon-javascript-plain text-9xl text-white"></i></li>
+                  <li className="mb-0 mt-1 font-medium text-lg italic"><label htmlFor='JS'>Java script</label></li>
+                  <li><input id='JS' type="checkbox" value='JS' /></li>
+                </ul>
               </div>
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-csharp-plain text-9xl text-white"></i>
-                <label htmlFor='C#'>C#</label>
-                <input id='C#' type="checkbox" value='C#' />
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6">
+                <ul className="text-center">
+                  <li><i className="devicon-csharp-plain text-9xl text-white"></i></li>
+                  <li className="mb-0 mt-1 font-medium text-lg italic"><label htmlFor='C#'>C#</label></li>
+                  <li><input id='C#' type="checkbox" value='C#' /></li>
+                </ul>
               </div>
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-typescript-plain text-9xl text-white"></i>
-                <label htmlFor='TS'>TS</label>
-                <input id='TS' type="checkbox" value='TS' />
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6">
+                <ul className="text-center">
+                  <li><i className="devicon-typescript-plain text-9xl text-white"></i></li>
+                  <li className='mb-0 mt-1 font-medium text-lg italic'><label htmlFor='TS'>TS</label></li>
+                  <li><input id='TS' type="checkbox" value='TS' /></li>
+                </ul>
               </div>
             </span>
 
@@ -38,23 +44,28 @@ function MoreInfoSignup () {
 
             <span className="flex flex-row">
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-ie10-original  text-9xl text-white"></i>
-                <label htmlFor='web'>Web</label>
-                <input id='web' type="checkbox" value='web'/>
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6  flex">
+                <ul className="text-center">
+                  <li><i className="devicon-ie10-original  text-9xl text-white"></i></li>
+                  <li className="mb-0 mt-1 font-medium text-lg italic"><label htmlFor='web'>Web</label></li>
+                  <li><input id='web' type="checkbox" value='web'/></li>
+                </ul>
               </div>
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-apple-original text-9xl text-white"></i>
-                <label htmlFor='mobile'>Mobile</label>
-                <input id='mobile' type="checkbox" value='mobile'/>
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6">
+                <ul className="text-center">
+                  <li><i className="devicon-apple-original text-9xl text-white"></i></li>
+                  <li className="mb-0 mt-1 font-medium text-lg italic"> <label htmlFor='mobile'>Mobile</label></li>
+                  <li><input id='mobile' type="checkbox" value='mobile'/></li>
+                </ul>
               </div>
 
-              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 border-black border-2 rounded-md m-6">
-                <i className="devicon-windows8-original text-9xl text-white"></i>
-
-                <label htmlFor='software'>Software</label>
-                <input id='software' type="checkbox" value='software'/>
+              <div className="w-52 h-52 bg-blue-200 hover:bg-blue-500 rounded-md m-6 p-6">
+                <ul className="text-center">
+                  <li><i className="devicon-windows8-original text-9xl text-white"></i></li>
+                  <li className="mb-0 mt-1 font-medium text-lg italic"><label htmlFor='software'>Software</label></li>
+                  <li><input id='software' type="checkbox" value='software'/></li>
+                </ul>
               </div>
 
               {/* currently this link is a placeholder for the route */}
