c5c143b5f02bff2cf16583e226630eb0dc83bcb4
tausani-ah-chong <tausani93@gmail.com>
2021-02-28 15:15:15 +1300

    edit app

--- client/App.jsx
+++ client/App.jsx
@@ -11,16 +11,18 @@ import Template from './components/Template'
 import SearchBar from './components/SearchBar'
 
 function App () {
-  <>
-    <Route path='/' component={Nav} />
-    <Route path='/' component={Banner} />
-    <Route path='/' component={SearchBar} />
-    <Route exact path='/' component={Explore} />
-    <Route exact path='/mobile' component={Mobile} />
-    <Route exact path='/web' component={Web} />
-    <Route exact path='/software' component={Software} />
-    <Route exact path='/css' component={Template} />
-  </>
+  return (
+    <>
+      <Route path='/' component={Nav} />
+      <Route path='/' component={Banner} />
+      <Route path='/' component={SearchBar} />
+      <Route exact path='/' component={Explore} />
+      <Route exact path='/mobile' component={Mobile} />
+      <Route exact path='/web' component={Web} />
+      <Route exact path='/software' component={Software} />
+      <Route exact path='/css' component={Template} />
+    </>
+  )
 }
 
 export default App
