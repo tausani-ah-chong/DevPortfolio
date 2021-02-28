d9fa4a79824c1383e96bee12ba3817ab49e192f8
Christo Karadjov <clkaradjov@gmail.com>
2021-03-01 10:01:03 +1300

    merging language grid into style on 1 march

--- client/components/PersonalSignup.jsx
+++ client/components/PersonalSignup.jsx
@@ -24,38 +24,42 @@ function PersonalSignup () {
 
   return (
     <>
-      <div>
+
+      <div className="w-full bg-blue-100 px-auto">
         <h1>Welcome! Let&apos;s create your profile</h1>
-      </div>
+        <h2>Add your details:</h2>
 
-      <div>
-
-        <div>
-          <h2>Add your details:</h2>
-        </div>
-
-        <div>
+        <div className="w-auto bg-blue-400 flex flex-col">
           <form onSubmit={handleSubmit}>
 
-            <label htmlFor='firstName'>First Name</label>
-            <input id='firstName' type='text' name='firstName' value={form.firstName} onChange={onChange} required />
-
-            <label htmlFor='lastName'>Last Name</label>
-            <input id='lastName' type='text' name='lastName' value={form.lastName} onChange={onChange} required />
+            <div className="listItem" >
+              <label htmlFor='firstName'>First Name</label>
+              <input id='firstName' type='text' name='firstName' value={form.firstName} onChange={onChange} required />
+            </div>
+            <div className="listItem">
+              <label htmlFor='lastName'>Last Name</label>
+              <input className="bg-gray-100 rounded-md" id='lastName' type='text' name='lastName' value={form.lastName} onChange={onChange} required />
+            </div>
 
             {/* Pronoun might be a checkbox
                 She/Her
                 He/Him
                 They/Them
             */}
-            <label htmlFor='pronoun'>Pronoun</label>
-            <input id='pronoun' type='text' name='pronoun' value={form.pronoun} onChange={onChange} required />
+            <div className="listItem">
+              <label htmlFor='pronoun'>Pronoun</label>
+              <input id='pronoun' type='text' name='pronoun' value={form.pronoun} onChange={onChange} required />
+            </div>
 
-            <label htmlFor='profilePicture'>Add a Profile Picture</label>
-            <input id='profilePicture' type='file' name='profilePicture' accept="image/png, image/jpeg" required />
+            <div className="listItem">
+              <label htmlFor='profilePicture'>Add a Profile Picture</label>
+              <input id='profilePicture' type='file' name='profilePicture' accept="image/png, image/jpeg" required />
+            </div>
 
-            <label htmlFor='bio'>Bio</label>
-            <input id='bio' type='text' name='bio' value={form.bio} onChange={onChange} required />
+            <div className="listItem">
+              <label htmlFor='bio'>Bio</label>
+              <input id='bio' type='text' name='bio' value={form.bio} onChange={onChange} required />
+            </div>
 
             <Link to='/more'>
               <button>Next</button>
