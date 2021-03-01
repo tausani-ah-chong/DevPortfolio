7670a66dc04fead799c0a982f34ccd35a39b09f6
steve-kim-96 <soungku@hotmail.com>
2021-03-01 21:17:00 +1300

    fix personal signup

--- client/components/PersonalSignup.jsx
+++ client/components/PersonalSignup.jsx
@@ -47,12 +47,6 @@ function PersonalSignup (props) {
                 <input className="inputBox h-8 text-xl " placeholder="Last Name:"id='lastName' type='text' name='lastName' value={form.lastName} onChange={onChange} required />
                 <hr className="border-black mb-4 "></hr>
               </div>
-
-              {/* Pronoun might be a checkbox
-                She/Her
-                He/Him
-                They/Them
-            */}
               <div className="inputDiv">
                 <input className="inputBox h-8 text-xl " placeholder="Pronouns" id='pronoun' type='text' name='pronoun' value={form.pronoun} onChange={onChange} required />
                 <hr className="border-black mb-4 "></hr>
@@ -64,11 +58,11 @@ function PersonalSignup (props) {
               </div>
 
               <div className="inputDiv">
-                <input className="inputBox h-8 text-xl " placeholder="Profile Picture" id='profilePicture' type='file' name='profilePicture' accept="image/png, image/jpeg" required />
+                <input className="inputBox h-8 text-xl " placeholder="Profile Picture" id='profilePicture' type='file' name='profilePicture' accept="image/png, image/jpeg" />
               </div>
 
               <p>Profile Picture</p>
-              <button type="button" className="my-6 flex items-center mr-auto hover:bg-yellow-200 bg-blue-200 rounded-md h-10 px-3 font-semibold text-lg">Next</button>
+              <button className="my-6 flex items-center mr-auto hover:bg-yellow-200 bg-blue-200 rounded-md h-10 px-3 font-semibold text-lg">Next</button>
 
             </form>
 
