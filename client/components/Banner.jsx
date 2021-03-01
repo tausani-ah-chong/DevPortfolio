25c240d3fb9d4440b0cee31aa71ce217dc3b1105
Christo Karadjov <clkaradjov@gmail.com>
2021-03-02 11:41:46 +1300

    image in homepage banner is 16.9 aspect ratio

--- client/components/Banner.jsx
+++ client/components/Banner.jsx
@@ -10,9 +10,13 @@ function Banner () {
         <h4 className="xl:text-2xl lg:text-xl font-medium my-6"> DevPortfolio, connecting those who want with those who need</h4>
         <button type="button" className="buttonNoMargin">Sign up</button>
       </div>
-      <div className="order-2 w-full h-full">
-        {/* <img className="rounded-md h-96 w-auto" src='/images/placeholder-img/pic1.jpg'/> */}
-        <TemplateProfileCarousel/>
+      <div className="order-2 aspect-w-16 aspect-h-9 rounded-md">
+        {/* <TemplateProfileCarousel/> */}
+        <div className="h-0 pt-p">
+          <img className="h-5/6 w-5/6 absolute top-0 left-0 lg:ml-12 xl:ml-10 lg:mt-5 object-cover rounded-md" src='/images/placeholder-img/pic1.jpg'/>
+
+          {/* <div className="bg-green-400  h-5/6 w-5/6 absolute top-0 left-0 lg:ml-12 xl:ml-10 lg:mt-5 rounded-md"></div> */}
+        </div>
       </div>
     </div>
   )
