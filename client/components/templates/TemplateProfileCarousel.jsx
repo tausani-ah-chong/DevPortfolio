45e581d165d5e97a4d4fa2e215852f41c0c4a960
tausani-ah-chong <tausani93@gmail.com>
2021-03-02 12:00:57 +1300

    change a tag to span in authsignup

--- client/components/templates/TemplateProfileCarousel.jsx
+++ client/components/templates/TemplateProfileCarousel.jsx
@@ -5,8 +5,6 @@ export default function TemplateProfileCarousel () {
 
   // setTimeout(function () { setPic(pic + 1) }, 1000)
 
-  console.log(pic)
-
   if (pic > 3) {
     setPic(1)
   } else if (pic < 1) {
@@ -16,9 +14,9 @@ export default function TemplateProfileCarousel () {
   return (
     <div className="mr-96 xl:h-96 w-auto flex items-center">
       {/* <div className="w-c flex items-center"> */}
-        {/* <img className=" w-14 h-auto opacity-20" src='/images/placeholder-img/left.png' onClick={() => setPic(pic - 1)}/> */}
-        <img src={`/images/placeholder-img/pic${pic}.jpg`} className="object-cover rounded-md"/>
-        {/* <img className=" w-14 h-auto opacity-20" src='/images/placeholder-img/right.png' onClick={() => setPic(pic + 1)}/> */}
+      {/* <img className=" w-14 h-auto opacity-20" src='/images/placeholder-img/left.png' onClick={() => setPic(pic - 1)}/> */}
+      <img src={`/images/placeholder-img/pic${pic}.jpg`} className="object-cover rounded-md"/>
+      {/* <img className=" w-14 h-auto opacity-20" src='/images/placeholder-img/right.png' onClick={() => setPic(pic + 1)}/> */}
       {/* </div> */}
     </div>
   )
