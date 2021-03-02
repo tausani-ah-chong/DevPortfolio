5083483c1541687db7f9e76e0d8a5e35ee427814
Christo Karadjov <clkaradjov@gmail.com>
2021-03-02 14:52:56 +1300

    re styled search bar buttons

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -5,26 +5,33 @@ function SearchBar () {
   return (
     <div className="h-14 w-full flex justify-center items-center">
       <Link to='/'>
-        <button type="button" className="defaultButton">All Devs</button>
+        <button type="button" className="buttonSearchBar">All Devs</button>
       </Link>
+
       <Link to='/mobile'>
-        <button type="button" className="defaultButton">Mobile</button>
+        <button type="button" className="buttonSearchBar">Mobile</button>
       </Link>
+
       <Link to='/web'>
-        <button type="button" className="defaultButton">Web</button>
+        <button type="button" className="buttonSearchBar">Web</button>
       </Link>
+
       <Link to='/software'>
-        <button type="button" className="defaultButton">Software</button>
+        <button type="button" className="buttonSearchBar">Software</button>
       </Link>
+
       <Link to='/js'>
-        <button type="button" className="defaultButton">JS</button>
+        <button type="button" className="buttonSearchBar">JS</button>
       </Link>
+
       <Link to='/ts'>
-        <button type="button" className="defaultButton">TS</button>
+        <button type="button" className="buttonSearchBar">TS</button>
       </Link>
+
       <Link to='/c'>
-        <button type="button" className="defaultButton">C#</button>
+        <button type="button" className="buttonSearchBar">C#</button>
       </Link>
+
     </div>
   )
 }
