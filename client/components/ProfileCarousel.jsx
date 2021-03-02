a263c6cccf67cceb5214c9182b5e00eca50d7e2f
Christo Karadjov <clkaradjov@gmail.com>
2021-03-02 13:24:57 +1300

    moving out of branch

--- client/components/ProfileCarousel.jsx
+++ client/components/ProfileCarousel.jsx
@@ -3,12 +3,16 @@ import React from 'react'
 export default function ProfileCarousel () {
   return (
     <>
-      <div className="w-full order-2 aspect-w-16 aspect-h-9">
-        {/* <TemplateProfileCarousel/> */}
-        <div className="h-0 pt-p">
-          <img className="h-4/6 w-4/6 absolute top-0 left-0 lg:ml-12 xl:ml-10 lg:mt-5 xl:mt-0 object-cover rounded-md" src='/images/placeholder-img/pic1.jpg'/>
-        </div>
+      <div className="flex flex-row items-center">
+        <div className="h-10 w-10 bg-red-400"></div>
+        <div className="w-full order-2 aspect-w-16 aspect-h-9">
+          {/* <TemplateProfileCarousel/> */}
+          <div className="h-0 pt-p">
+            <img className="h-4/6 w-4/6 absolute top-0 left-0 lg:ml-12 xl:ml-10 lg:mt-5 xl:mt-0 object-cover rounded-md" src='/images/placeholder-img/pic1.jpg'/>
+          </div>
 
+        </div>
+        <div className="h-10 w-10 bg-red-400 mx-10"></div>
       </div>
     </>
 
