362b9422ab3800f64cd74c7a547fbda71ec30db7
Christo Karadjov <clkaradjov@gmail.com>
2021-03-02 16:55:29 +1300

    added and styled profile explore component

--- client/components/templates/NewProfile.jsx
+++ client/components/templates/NewProfile.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NewProfileBanner from './NewProfileBanner'
+import TemplateProfileExplore from './TemplateProfileExplore'
 
 export default function NewProfile () {
   return (
     <>
       <NewProfileBanner/>
+      <TemplateProfileExplore/>
     </>
   )
 }
