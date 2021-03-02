7107f5c59d3aa70103150738dba4c5b4fe11d33d
Christo Karadjov <clkaradjov@gmail.com>
2021-03-03 01:40:42 +1300

    landingpage styled and responsive

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 export default function LandingPage () {
   return (
     <>
-      <div className='h-screen w-full flex items-center lg:pb-32 '>
-        <div className='w-full lg:h-72  flex justify-center'>
-          <div className=' lg:w-96 ml-auto pt-10 '>
+      <div className='h-screen w-full flex items-center lg:pb-32 xl:pb-80 '>
+        <div className='w-full lg:h-72 xl:h-96 flex justify-center'>
+          <div className=' lg:w-96 lg:ml-auto lg:pt-10 xl:pt-20 '>
             <div className='flex'>
-              <button className='flex items-center bg-blue-200 rounded-md lg:h-10 px-3 lg:mr-1 lg:mt-1 text-white font-extrabold lg:text-4xl focus:outline-none'>Dev</button>
-              <h1 className='lg:text-5xl lg:font-extrabold mb-6'>Portfolio.</h1>
+              <button className='flex items-center bg-blue-200 rounded-md lg:h-10 xl:h-12 px-3 lg:mr-1 lg:mt-1 text-white font-extrabold lg:text-4xl focus:outline-none'>Dev</button>
+              <h1 className='lg:text-5xl xl:text-6xl lg:font-extrabold lg:mb-6'>Portfolio.</h1>
             </div>
-            <h1 className='lg:text-3xl font-bold mb-6'>welcome to the future</h1>
+            <h1 className='lg:text-3xl xl:text-4xl font-bold lg:mb-6'>welcome to the future</h1>
             <h2 className='lg:text-xl font-semibold'>we help developers connect with the world</h2>
-            <button className='buttonNoMargin mt-3'>Get Started</button>
+            <button className='flex items-center hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 xl:h-10 lg:mt-3 xl:mt-5 px-3 lg:font-medium xl:font-semibold xl:text-lg lg:text-base focus:outline-none '>Get Started</button>
           </div>
           <div className='mr-auto'>
-            <img className='lg:h-72 w-auto' src='/images/placeholder-img/landingPage.png'></img>
+            <img className='lg:h-72 xl:h-96 w-auto' src='/images/placeholder-img/landingPage.png'></img>
           </div>
         </div>
 
