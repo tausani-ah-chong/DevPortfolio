5ea635caf5809e741c64582106dd9a8240365331
Christo Karadjov <clkaradjov@gmail.com>
2021-03-03 16:51:23 +1300

    touching up styling on landing page

--- client/components/LandingPage.jsx
+++ client/components/LandingPage.jsx
@@ -9,10 +9,10 @@ export default function LandingPage () {
           <div className=' lg:w-96 lg:ml-auto lg:pt-10 xl:pt-20 '>
             <div className='flex'>
               <span disabled='true' className='flex items-center bg-blue-200 rounded-md lg:h-10 xl:h-12 px-3 lg:mr-1 lg:mt-1 text-white font-extrabold lg:text-4xl focus:outline-none'>Dev</span>
-              <h1 className='lg:text-5xl xl:text-6xl lg:font-extrabold lg:mb-6'>Portfolio.</h1>
+              <h1 className='lg:text-5xl xl:text-6xl lg:font-extrabold lg:mb-1'>Portfolio.</h1>
             </div>
-            <h1 className='lg:text-3xl xl:text-4xl font-bold lg:mb-6'>welcome to the future</h1>
-            <h2 className='lg:text-xl font-semibold'>we help developers connect with the world</h2>
+            <h1 className='lg:text-3xl xl:text-4xl font-bold lg:mb-3'>welcome to the future</h1>
+            <h2 className='lg:text-xl font-semibold mt-8'>we help developers connect with the world</h2>
             <Link to='/explore'>
               <button className='flex items-center hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 xl:h-10 lg:mt-3 xl:mt-5 px-3 lg:font-medium xl:font-semibold xl:text-lg lg:text-base focus:outline-none '>Explore</button>
             </Link>
