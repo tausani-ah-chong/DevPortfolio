b749c9f0d5776fc5f29724dde42547b770430fb5
Christo Karadjov <clkaradjov@gmail.com>
2021-03-03 15:53:37 +1300

    log out button centered in nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -23,18 +23,20 @@ function Nav () {
               <h1 className="xl:text-xl lg:text-lg font-extrabold my-auto hover:text-blue-400">DevPortfolio</h1>
             </Link>
           </div>
-          {currentUser ? <span className="flex relative order-1"><button onClick={handleLogOut} className="buttonBlue absolute right-0 xl:text-lg lg:text-base">Log Out</button></span>
-            : <span className="flex relative order-1">
-              <div className="flex items-center ml-auto">
-                <Link to='/login'>
-                  <button className="lg:font-medium xl:font-semibold mx-6 inline hover:text-blue-400 lg:text-base">Sign in</button>
-                </Link>
-                <Link to='/signup'>
-                  <button className="flex items-center xl:ml-2 hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 px-3 lg:font-medium xl:font-semibold focus:outline-none lg:text-base">Sign Up</button>
-                </Link>
-              </div>
-            </span>
-          }
+          <div>
+            {currentUser ? <span className="flex relative order-1"><div className="flex items-center ml-auto"><button onClick={handleLogOut} className="flex items-center xl:ml-2 hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 px-3 lg:font-medium xl:font-semibold focus:outline-none lg:text-base">Log Out</button></div></span>
+              : <span className="flex relative order-1">
+                <div className="flex items-center ml-auto">
+                  <Link to='/login'>
+                    <button className="lg:font-medium xl:font-semibold mx-6 inline hover:text-blue-400 lg:text-base">Sign in</button>
+                  </Link>
+                  <Link to='/signup'>
+                    <button className="flex items-center xl:ml-2 hover:bg-blue-400 bg-blue-200 rounded-md lg:h-8 px-3 lg:font-medium xl:font-semibold focus:outline-none lg:text-base">Sign Up</button>
+                  </Link>
+                </div>
+              </span>
+            }
+          </div>
         </div>
       </nav>
 
